404cd0e0e2405dcd431ad3a8632f50efe9b9e533
pulseDetector <40602292+pulseDetector@users.noreply.github.com>
2021-03-17 00:09:08 +0530

    Update ProductMSApplicationTests.java

--- src/test/java/com/infy/product/ProductMSApplicationTests.java
+++ src/test/java/com/infy/product/ProductMSApplicationTests.java
@@ -1,5 +1,11 @@
 package com.infy.product;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ProductMSApplicationTests {
 
 }
